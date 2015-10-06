f1df1c3c0896832a4441575980f88f562a56a4cd
JSnake <plinky92092@gmail.com>
2015-10-05 21:58:31 -0400

    PSet 2 Part 1

--- Introduction/MIT 6001x/Week 2/Problem Set 2/counting_bobs.py
+++ Introduction/MIT 6001x/Week 2/Problem Set 2/counting_bobs.py
@@ -1,8 +1,8 @@
 s = "azcbobobegghakl"
 bob_count = 0
 
-for i in range(1, len(s) - 1):
-    if s[i - 1: i + 2] == "bob":
+for i in s:
+    if "bob" in i:
         bob_count += 1
 
 print "Number of times bob occurs is:", bob_count
